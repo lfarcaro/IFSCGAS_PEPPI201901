b48e1317968d8d3bde23d1f23cdf62f1b4dcbd48
TharlisonSilva <tharlison.pro@gmail.com>
2019-05-10 19:49:03 -0300

    AJUSTE DA PÁGINA DE CONTATOS

--- website/js/main.js
+++ website/js/main.js
@@ -7,5 +7,14 @@
             if(text.length > 600)
                 el.text(text.substring(0, 596) + '...');
         });
+
+        Mascara_Telefone();
+        
     });
 })(jQuery);
+
+
+
+function Mascara_Telefone() {
+	$("#inputTelefone").mask("(00) 0000-0000");
+}
