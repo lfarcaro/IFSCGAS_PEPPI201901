bf980033692416240847b037980cba7a66e8303f
lfarcaro <48957854+lfarcaro@users.noreply.github.com>
2019-05-13 18:55:37 -0300

    Conteúdo comentado.

--- admin/composer_bake.bat
+++ admin/composer_bake.bat
@@ -1,11 +1,11 @@
-SET PATH=%PATH%;C:\xampp\php
-call bin\cake bake all categorias --force
-call bin\cake bake all administradores --force
-call bin\cake bake all designers --force
-call bin\cake bake all paginas --force
-call bin\cake bake all artigos --force
-call bin\cake bake all projetos --force
-call bin\cake bake all desfiles --force
-call bin\cake bake model artigo_fotografias --force
-call bin\cake bake model projeto_fotografias --force
-call bin\cake bake model desfile_fotografias --force
+%REM SET PATH=%PATH%;C:\xampp\php
+%REM call bin\cake bake all categorias --force
+%REM call bin\cake bake all administradores --force
+%REM call bin\cake bake all designers --force
+%REM call bin\cake bake all paginas --force
+%REM call bin\cake bake all artigos --force
+%REM call bin\cake bake all projetos --force
+%REM call bin\cake bake all desfiles --force
+%REM call bin\cake bake model artigo_fotografias --force
+%REM call bin\cake bake model projeto_fotografias --force
+%REM call bin\cake bake model desfile_fotografias --force
