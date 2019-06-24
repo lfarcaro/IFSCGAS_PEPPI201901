34a5c398b75a9b7dcd4c57230d6a797651436a95
lfarcaro <48957854+lfarcaro@users.noreply.github.com>
2019-06-24 15:21:53 -0300

    Correção de falha de conexão.

--- api/conexao.php
+++ api/conexao.php
@@ -2,7 +2,7 @@
 
 class Conexao
 {
-	private $conexao = "mysql:dbname=PE_PPI_201901;host=peppi.gaspar.ifsc.edu.br;charset=utf8";
+	private $conexao = "mysql:dbname=PE_PPI_201901;host=192.168.0.3;charset=utf8";
 	private $user = "PE_PPI_201901";
 	private $password = "P3PPi201901";
 	function __construct()
