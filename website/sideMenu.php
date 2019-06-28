cdd5429bd7ad3b587144047adcb593ba4fd40569
menantes <emanoelmbueno@gmail.com>
2019-06-28 20:47:00 -0300

    Grupo 1 - Arquivos php

--- website/sideMenu.php
+++ website/sideMenu.php
@@ -1,47 +1,45 @@
-<?php
-	echo'<div class="row">
-			<!--MENU LATERAL-->
-			<div id="side-menu" class="col-lg-2 pl-0 pr-0 navbar-dark bg-dark">
-				<nav class="navbar navbar-expand-lg navbar-dark bg-dark  pl-0 pr-0 ">
-					<div class="collapse navbar-collapse  pl-0 pr-0 " id="navbarNav">
-				    	<ul class="navbar-nav flex-column col-lg-12  pl-0 pr-0 ">
-				      		<li class="nav-item">
-				        		<a class="nav-link" onclick="reloadContent(this);">Ateliê</a>	
-								<hr>
-				      		</li>
-							
-				      		<li class="nav-item">
-				        		<a class="nav-link" onclick="reloadContent(this);">Designers</a>
-								<hr>
-				    		</li>
-							
-				    		<li class="nav-item">
-				    	    	<a class="nav-link" onclick="reloadContent(this);">Projetos</a>
-								<hr>
-				    		</li>
-							
-				    		<li class="nav-item">
-				    	    	<a class="nav-link" onclick="reloadContent(this);">Desfiles</a>
-								<hr>
-				    		</li>
-							
-				    		<li class="nav-item">
-				    	    	<a class="nav-link" onclick="reloadContent(this);">Revistas</a>
-								<hr>
-				    		</li>
-							
-				    		<li class="nav-item">
-				    	    	<a class="nav-link" onclick="reloadContent(this);">Sobre Nós</a>
-								<hr>
-				    		</li>
-							
-				    		<li class="nav-item">
-				    	    	<a class="nav-link" onclick="reloadContent(this);">Contato</a>
-								<hr>
-				    		</li>
-							
-				    	</ul>
-					</div>
-				</nav>
-			</div>'
-?>
+<div id="row" class="row">
+<!--MENU LATERAL-->
+<div id="side-menu" class="col-lg-2 pl-0 pr-0 navbar-dark bg-dark">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark  pl-0 pr-0 ">
+		<div class="collapse navbar-collapse  pl-0 pr-0 " id="navbarNav">
+			<ul class="navbar-nav flex-column col-lg-12  pl-0 pr-0 ">
+				<li class="nav-item">
+					<a class="nav-link" onclick="reloadContent(this);">Ateliê</a>	
+					<hr>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" onclick="reloadContent(this);">Designers</a>
+					<hr>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" onclick="reloadContent(this);">Projetos</a>
+					<hr>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" onclick="reloadContent(this);">Desfiles</a>
+					<hr>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" onclick="reloadContent(this);">Revistas</a>
+					<hr>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" onclick="reloadContent(this);">Sobre Nós</a>
+					<hr>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" onclick="reloadContent(this);">Contato</a>
+					<hr>
+				</li>
+				
+			</ul>
+		</div>
+	</nav>
+</div>
